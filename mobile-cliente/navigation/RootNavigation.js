e54670e4c8ed78caacf165711eedd184996e111d
Pato <cayo123fc@gmail.com>
2025-05-30 20:19:51 -0300

    Adição de Paginas E Problemas
    Foram adicionadas as Paginas Cadastro, Perfil e Filtragem Avançada, além de outros assets, infelizmente essas paginas estão sem a fonte montserrar

--- mobile-cliente/navigation/RootNavigation.js
+++ mobile-cliente/navigation/RootNavigation.js
@@ -5,17 +5,24 @@ import Cadastro from "../screens/Cadastro";
 import PagInicial from "../screens/PagInicial";
 import EvAbertos from "../screens/EvAbertos"; 
 import MeusEv from "../screens/MeusEv";
+import Perfil from "../screens/Perfil";
+import FiltroAvan from "../screens/FiltroAvan";
 const Stack = createStackNavigator();
 
 const RootNavigator = () => {
+
+
   return (
+
+
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{ headerShown: false }}
-        initialRouteName="Login"
-      >
+        initialRouteName="Login">
         <Stack.Screen name="Login" component={Login}></Stack.Screen>
         <Stack.Screen name="Cadastro" component={Cadastro}></Stack.Screen>
+        <Stack.Screen name="FiltroAvan" component={FiltroAvan}></Stack.Screen>
+        <Stack.Screen name="Perfil" component={Perfil}></Stack.Screen>        
         <Stack.Screen name="PagInicial" component={PagInicial}></Stack.Screen>
         <Stack.Screen name="EvAbertos" component={EvAbertos}></Stack.Screen>
         <Stack.Screen name="MeusEv" component={MeusEv}></Stack.Screen>
